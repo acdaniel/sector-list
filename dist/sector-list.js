9569f7c0c650d74e1813ad529d2ebcb09e8bc9c5
Adam Daniel <adam@acdaniel.com>
2014-04-03 14:56:22 -0400

    adds example

--- dist/sector-list.js
+++ dist/sector-list.js
@@ -6,7 +6,7 @@
  * Copyright 2014 Adam Daniel <adam@acdaniel.com>
  * Released under the MIT license
  *
- * Date: 2014-04-03T01:50:49.433Z
+ * Date: 2014-04-03T18:21:29.747Z
  */
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self);var f=o;f=f.sector||(f.sector={}),f=f.ext||(f.ext={}),f.list=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 var sector; try { sector = _dereq_('sector'); } catch (e) { sector = window.sector; }
@@ -128,7 +128,7 @@ module.exports =  function Selectable () {
       return this._selected;
     },
     set: function (selected) {
-      if (this._selected !== selected) {
+      if (this.el && this._selected !== selected) {
         this._selected = selected;
         var e = utils.createEvent('selected', { selected: selected });
         this.el.dispatchEvent(e);
